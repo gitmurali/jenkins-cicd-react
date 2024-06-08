70c6542858fb6dbb5fd8585fb8b226858290019b
Murali ⚡️ <murali.nutalapatii@gmail.com>
2024-06-08 17:08:55 +0530

    update app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME TO jenkins project
+          WELCOME TO AWS CLOUD DEMOS
         </p>
         <a
           className="App-link"
